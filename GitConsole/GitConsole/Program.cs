23206da886ba3467704c6d94ae01ddc02eab3e5f
axkhan2 <arif788@gmail.com>
2014-01-26 17:25:11 +0530

    Added conflicted console line from system

--- GitConsole/GitConsole/Program.cs
+++ GitConsole/GitConsole/Program.cs
@@ -12,6 +12,7 @@ namespace GitConsole
         {
             Console.WriteLine("Hello World!!!");
             Console.WriteLine("This line wass added from github ui .com");
+            Console.WriteLine("This line has conflict");
         }
     }
 }
